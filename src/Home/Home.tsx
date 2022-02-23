14305d9864ebcd45c82aef61c50c46abcf0e5b2f
Chetan Chudasama <chetanit2013@gmail.com>
2022-02-23 13:06:14 +0530

    added header

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import "./Home.css";
+import MenuBar from "../MenuBar/MenuBar";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className="home">
-        <h1> Home Page</h1>
-      </div>
+      <MenuBar />
     </>
   );
 };
