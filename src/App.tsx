585c7482d5f124db9a1726ededbcd1ea32630f1a
Chetan Chudasama <chetanit2013@gmail.com>
2022-02-23 19:29:49 +0530

    implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
-import Home from "./components/Home/Home";
+import MenuBar from "./components/MenuBar/MenuBar";
+import SellItem from "./components/SellItem/SellItem";
+import { Switch, Route } from "react-router-dom";
+import ItemPostList from "./components/ItemPostList/ItemPostList";
+import Post from "./components/Post/Post";
 
 function App() {
   return (
-    <div className="">
-      <Home />
+    <div>
+      <MenuBar />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={ItemPostList} />
+          <Route exact path="/add-post" component={SellItem} />
+          <Route exact path="/post-view/:id" component={Post} />
+        </Switch>
+      </div>
     </div>
   );
 }
